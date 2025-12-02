c9e3b0e050e64e21b39863dbe69e994d190409cf
sakshi khatavkar <140887464+Sakshii49@users.noreply.github.com>
2025-12-02 11:04:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { ThemeProvider } from './context/ThemeContext';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div className="app">
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Experience />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
